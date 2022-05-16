066c2f4f0792b110c72f312b50d4a536bde686af
Zuam7 <thuanromero@gmail.com>
2022-05-16 17:55:01 -0500

    second commit

---
+++ FormsApp/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace FormsApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public void PrintInfo(object sender, EventArgs e)
+        {
+            info1.Text = fname.Text;
+            info2.Text = lname.Text;
+            info3.Text = pnumber.Text;
+            info4.Text = email.Text;
+            info5.Text = address.Text;
+        }
+    }
+}
---
+++ FormsApp/results.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FormsApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class results : ContentPage
+    {
+        public results()
+        {
+            InitializeComponent();
+        }
+    }
+}
